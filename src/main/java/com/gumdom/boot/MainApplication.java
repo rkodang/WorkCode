e4042aefaae9c67d710da5bbccd2cfbab18ad3ec
rkodang <245894317@qq.com>
2023-04-23 22:36:31 +0800

    1.今晚收工了

--- src/main/java/com/gumdom/boot/MainApplication.java
+++ src/main/java/com/gumdom/boot/MainApplication.java
@@ -14,10 +14,5 @@ public class MainApplication {
         ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class);
         //2.查看容器里面的组件
         String[] beanDefinitionNames = run.getBeanDefinitionNames();
-        String formattedInt = Integer.toHexString((int) (System.currentTimeMillis() >>> 8));
-        StringBuilder builder = new StringBuilder("0000000");
-        builder.replace(8-formattedInt.length(),8,formattedInt);
-        System.err.println(builder.toString());
-
     }
 }
